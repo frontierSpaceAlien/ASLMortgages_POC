fc6a67c387d891f57d3c86b108786f52a6a60a27
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2023-03-03 23:41:52 +1300

    Added borrower table

--- src/pages/Borrower.js
+++ src/pages/Borrower.js
@@ -1,13 +1,168 @@
-import React, { Component } from "react";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-class Borrower extends Component {
-  render() {
+function createData(name, startDate, endDate, dayIntDue, intRate, price) {
+  return {
+    name,
+    startDate,
+    endDate,
+    dayIntDue,
+    intRate,
+    price,
+    history: [
+      {
+        date: '2020-01-05',
+        customerId: '11091700',
+        amount: 3,
+      },
+      {
+        date: '2020-01-02',
+        customerId: 'Anonymous',
+        amount: 1,
+      },
+    ],
+  };
+}
 
-    return (
-      <div className="slideView">
-        <h2>borrower</h2>
-      </div>
-    );
-  }
-} 
-export default Borrower;
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.startDate}</TableCell>
+        <TableCell align="right">{row.endDate}</TableCell>
+        <TableCell align="right">{row.dayIntDue}</TableCell>
+        <TableCell align="right">{row.intRate}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="right">{historyRow.amount}</TableCell>
+                      <TableCell align="right">
+                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    startDate: PropTypes.number.isRequired,
+    endDate: PropTypes.number.isRequired,
+    dayIntDue: PropTypes.number.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    intRate: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const rows = [
+  createData('Borrower 1', 159, 6.0, 10, 14.95, 3.99),
+  createData('Borrower 2', 237, 9.0, 13, 15.95, 4.99),
+  createData('Borrower 3', 262, 16.0, 6, 15.00, 3.79),
+  createData('Borrower 4', 305, 3.7, 19, 12.95, 2.5),
+  createData('Borrower 5', 356, 16.0, 25, 13.95, 1.5),
+  createData('Borrower 6', 356, 16.0, 1, 15.00, 1.5),
+]
+
+export default function CollapsibleTable() {
+return (
+  <div className = "tableView">
+    <Paper elevation={6} style={{ height: "100%", paddingLeft: 100, paddingRight:100}}>
+        <TableContainer>
+          <h2>Borrower Information</h2>
+          <Table  aria-label="collapsible table" size='small'>
+            <TableHead>
+              <TableRow>
+                <StyledTableCell />
+                <StyledTableCell>Borrower</StyledTableCell>
+                <StyledTableCell align="right">Start Date</StyledTableCell>
+                <StyledTableCell align="right">End Date</StyledTableCell>
+                <StyledTableCell align="right">Day Interest Due</StyledTableCell>
+                <StyledTableCell align="right">Interest Rate %</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <Row key={row.name} row={row} />
+                ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    </Paper>
+  </div>
+  );
+}
