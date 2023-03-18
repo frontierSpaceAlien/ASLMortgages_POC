d1afaf187a1a84b0559f732dee8721e5a8fcabe9
Wenxi Qiao <84102516+alexqiao860315@users.noreply.github.com>
2023-03-18 20:59:29 +1300

    Edit button works now

--- src/pages/Investor.js
+++ src/pages/Investor.js
@@ -84,7 +84,7 @@ function createData(borrowerID, name, loanAmount, intRate, dailyInterest, totalP
 /** DUMMY DATA **/
 // Never have duplicate borrower ids. Duplicate borrower ids will cause the table to break.
 const rows = [
-  createData(0,'Investor 1', 359066.74, 16.95, 100, 200000),
+  createData(0,'Investor 1', 0, 0, 0, 0),
 
 ]
 
@@ -193,6 +193,35 @@ export default function CollapsibleTable() {
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [rowData, setRowData] = React.useState(rows);
   const [modal, setModal] = React.useState(false);
+  const [editInvestorIndex, setEditInvestorIndex] = useState(null);
+  const [editInvestorModal, setEditInvestorModal] = useState(false);
+
+  const handleEditInvestorModalOpen = (index) => {
+    setEditInvestorIndex(index);
+    setInvestorName(rowData[index].name);
+    setInvestorAmount(rowData[index].loanAmount);
+    setInterestRate(rowData[index].intRate);
+    setEditInvestorModal(true);
+  };
+
+  const handleUpdateInvestor = () => {
+    const updatedInvestor = {
+      ...rowData[editInvestorIndex],
+      name: investorName,
+      loanAmount: parseFloat(investorAmount),
+      intRate: parseFloat(interestRate),
+      dailyInterest: parseFloat(investorAmount) * parseFloat(interestRate) / 100,
+    };
+  
+    const updatedRowData = [...rowData];
+    updatedRowData[editInvestorIndex] = updatedInvestor;
+    setRowData(updatedRowData);
+    setEditInvestorModal(false);
+  };
+
+  const handleEditInvestorModalClose = () => {
+    setEditInvestorModal(false);
+  };
 
   const handleAddInvestorModalOpen = () => {
     setAddInvestorModal(true);
@@ -262,12 +291,6 @@ const handleAddInvestor = () => {
 const [investorName, setInvestorName] = useState('');
 const [investorAmount, setInvestorAmount] = useState('');
 const [interestRate, setInterestRate] = useState('');
-
-// Calculate daily interest and total price
-const dailyInterest = investorAmount * interestRate / 100;
-const totalPrice = parseFloat(investorAmount) + dailyInterest;
-  
-
 
   return (
     <div className = "tableView">
@@ -341,10 +364,42 @@ const totalPrice = parseFloat(investorAmount) + dailyInterest;
                     <TableCell align="right">{row.totalPrice}</TableCell>
                     <TableCell align="right">
                         <Tooltip title="Edit">
-                          <IconButton>
+                        <IconButton onClick={() => handleEditInvestorModalOpen(dataIndex)}>
                             <EditIcon/>
                           </IconButton>
                         </Tooltip>
+                        <Dialog
+                          open={editInvestorModal}
+                          onClose={handleEditInvestorModalClose}
+                          aria-labelledby="edit-investor-dialog-title"
+                          aria-describedby="edit-investor-dialog-description"
+                        >
+                          <DialogTitle id="edit-investor-dialog-title">Edit Investor</DialogTitle>
+                          <DialogContent>
+                            <TextField
+                              label="Investor"
+                              value={investorName}
+                              onChange={(e) => setInvestorName(e.target.value)}
+                              fullWidth
+                            />
+                            <TextField
+                              label="Investor Amount"
+                              value={investorAmount}
+                              onChange={(e) => setInvestorAmount(e.target.value)}
+                              fullWidth
+                            />
+                            <TextField
+                              label="Interest Rate"
+                              value={interestRate}
+                              onChange={(e) => setInterestRate(e.target.value)}
+                              fullWidth
+                            />
+                          </DialogContent>
+                          <DialogActions>
+                            <Button onClick={handleEditInvestorModalClose}>Cancel</Button>
+                            <Button onClick={handleUpdateInvestor}>Save</Button>
+                          </DialogActions>
+                        </Dialog>
                         <Tooltip title="Delete">
                           <IconButton value={row.borrowerID} onClick={() => handlePopup(dataIndex)}>
                             <DeleteIcon color = "error"/>
@@ -402,129 +457,5 @@ const totalPrice = parseFloat(investorAmount) + dailyInterest;
 
 
 
-/*import React from 'react';
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
-/*const InvestorTable = () => {
-  // Dummy data
-  const investors = [
-    { name: 'John Doe', investment: 429258, interestRate: 0.15 },
-    { name: 'Jane Smith', investment: 457388, interestRate: 0.15 },
-    { name: 'Bob Johnson', investment: 553832, interestRate: 0.15 },
-  ];
-
-  const calculateDailyInterest = (investment, interestRate) => {
-    // Calculate daily interest
-    return (investment * interestRate) / 365;
-  };
-
-  const calculateTotalPrice = (investment, interestRate) => {
-    // Calculate total price (investment + interest)
-    const dailyInterest = calculateDailyInterest(investment, interestRate);
-    const totalInterest = dailyInterest * 365;
-    return investment + totalInterest;
-  };
-  */
-
-  
-  /*const calculateTotalPrice = (investment, interestRate) => {
-    // Calculate total price (investment + interest)
-    const dailyInterest = calculateDailyInterest(investment, interestRate);
-    const totalInterest = dailyInterest * 365;
-    return investment + totalInterest;
-  };
-
-  const calculateDailyInterest = (investment, interestRate) => {
-    // Calculate daily interest
-    return (investment * interestRate) / 365;
-  };
-
-  function createData(Investor, Investment, InterestRate, DailyInterest, TotalPrice) {
-    return {Investor, Investment, InterestRate, DailyInterest, TotalPrice};
-  }
-  
-  const rows = [
-    createData('Leo', 159, 6.0, 24, 4.0),
-    createData('Alex', 237, 9.0, 37, 4.3),
-    createData('Jason', 262, 16.0, 24, 6.0),
-    createData('Angus', 305, 3.7, 67, 4.3),
-    createData('Des', 356, 16.0, 49, 3.9),
-  ];
-  
-  export default function BasicTable() {
-    return (
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Investor</TableCell>
-              <TableCell align="right">Investment</TableCell>
-              <TableCell align="right">Interest rate</TableCell>
-              <TableCell align="right">Daily Interest</TableCell>
-              <TableCell align="right">Total Price</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.Investor}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.Investor}
-                </TableCell>
-                <TableCell align="right">{row.Investment}</TableCell>
-                <TableCell align="right">{row.InterestRate}</TableCell>
-                <TableCell align="right">{calculateDailyInterest(row.Investment, row.InterestRate)}</TableCell>
-                <TableCell align="right">{calculateTotalPrice(row.Investment, row.InterestRate)}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  }
-  
-
-
-
-
-  /*
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Investor</th>
-          <th>Investment</th>
-          <th>Interest Rate</th>
-          <th>Daily Interest</th>
-          <th>Total Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {investors.map((investor) => (
-          <tr key={investor.name}>
-            <td>{investor.name}</td>
-            <td>${investor.investment}</td>
-            <td>{investor.interestRate * 100}%</td>
-            <td>${calculateDailyInterest(investor.investment, investor.interestRate).toFixed(2)}</td>
-            <td>${calculateTotalPrice(investor.investment, investor.interestRate).toFixed(2)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-
-  
-};
-
-export default InvestorTable;
-*/
 
