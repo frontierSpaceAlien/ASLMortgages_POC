288061848cd6be94175dc4d2ad4c85d3381b2ca4
breanna <jwt5087@autuni.ac.nz>
2023-03-28 17:52:49 +1300

    backend - Added Investor CRUD Routes

--- server/databasq.sql
+++ server/databasq.sql
@@ -6,5 +6,14 @@ CREATE TABLE borrower(
     borrowerLastName VARCHAR(255),
     BorrowerStreetAddress VARCHAR(255),
     borrowerContactNumber VARCHAR(255)
+    borrowerEmailAddress VARCHAR(255)
 );
 
+CREATE TABLE investor(
+    investor_id SERIAL PRIMARY KEY,
+    investorFirstName VARCHAR(255),
+    investorLastName VARCHAR(255),
+    investorStreetAddress VARCHAR(255),
+    investorContactNumber VARCHAR(255),
+    investorEmailAddress VARCHAR(255)
+);
--- server/server.js
+++ server/server.js
@@ -10,14 +10,17 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 
-// ROUTES // GET All Borrowers - http://localhost:3006/borrower
+// ROUTES 
+// BORROWER ROUTES
+
+//Get all borrowers
 app.get("/borrower", async (req, res) => {
 
     try {
         const results = await db.query("SELECT * FROM borrower")
         console.log(results);
         res.status(200).json({
-            status: "success",
+            status: "success", 
             data: {
                 borrower: results.rows,
             },
@@ -32,7 +35,6 @@ app.get("/borrower/:id", async (req, res) => {
     //http://localhost:3006/borrower/:id
     try {
         const results = await db.query("SELECT * FROM borrower WHERE borrower_id = $1", [req.params.id]);
-        //console.log(results.rows[0]);
         res.status(200).json({
             status: "success",
             data: {
@@ -50,8 +52,8 @@ app.post("/borrower", async(req, res) => {
     console.log(req.body);
     try {
         const results = await db.query(
-            "INSERT INTO borrower (borrowerFirstName, borrowerLastName, borrowerStreetAddress, borrowerContactNumber) VALUES ($1, $2, $3, $4) returning *",
-                 [req.body.borrowerFirstName, req.body.borrowerLastName, req.body.borrowerStreetAddress, req.body.borrowerContactNumber]);
+            "INSERT INTO borrower (borrowerFirstName, borrowerLastName, borrowerStreetAddress, borrowerContactNumber, borrowerEmailAddress) VALUES ($1, $2, $3, $4, $5) returning *",
+                 [req.body.borrowerFirstName, req.body.borrowerLastName, req.body.borrowerStreetAddress, req.body.borrowerContactNumber, req.body.borrowerEmailAddress]);
         console.log(results);        
          res.status(201).json({
             status: "success",
@@ -68,12 +70,13 @@ app.post("/borrower", async(req, res) => {
 app.put("/borrower/:id", async (req, res) => {
     try {
         const results = await db.query(
-            "UPDATE borrower SET borrowerFirstName = $1, borrowerLastName = $2, borrowerStreetAddress = $3, borrowerContactNumber = $4 where borrower_id = $5 returning *", 
+            "UPDATE borrower SET borrowerFirstName = $1, borrowerLastName = $2, borrowerStreetAddress = $3, borrowerContactNumber = $4, borrowerEmailAddress = $5 where borrower_id = $6 returning *", 
             [
                 req.body.borrowerFirstName, 
                 req.body.borrowerLastName, 
                 req.body.borrowerStreetAddress, 
                 req.body.borrowerContactNumber,
+                req.body.borrowerEmailAddress,
                 req.params.id
             ]);
 
@@ -92,6 +95,97 @@ app.put("/borrower/:id", async (req, res) => {
 app.delete("/borrower/:id", async (req, res) => {
     try {
         const result = db.query("DELETE FROM borrower WHERE borrower_id = $1", [req.params.id]);
+        res.status(204).json({
+            status: "success", 
+        });
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+// INVESTOR ROUTES
+
+// Get all Investor Routes
+app.get("/investor", async (req, res) => {
+
+    try {
+        const results = await db.query("SELECT * FROM investor")
+        res.status(200).json({
+            status: "success",
+            data: {
+                investor: results.rows,
+            },
+        });  
+    } catch (err) {
+        console.error(err);        
+    }
+});
+
+//Get a Investor
+app.get("/investor/:id", async (req, res) => {
+    //http://localhost:3006/investor/:id
+    try {
+        const results = await db.query("SELECT * FROM investor WHERE investor_id = $1", [req.params.id]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                investor: results.rows[0],
+            },    
+        })
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+// Create a Investor
+app.post("/investor", async(req, res) => {
+    console.log(req.body);
+    try {
+        const results = await db.query(
+            "INSERT INTO investor (investorFirstName, investorLastName, investorStreetAddress, investorContactNumber, investorEmailAddress) VALUES ($1, $2, $3, $4, $5) returning *",
+                 [req.body.investorFirstName, req.body.investorLastName, req.body.investorStreetAddress, req.body.investorContactNumber, req.body.investorEmailAddress]);
+        console.log(results);        
+         res.status(201).json({
+            status: "success",
+            data: {
+                investor: results.rows[0],
+            },    
+        })
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+// Update a Investor 
+app.put("/investor/:id", async (req, res) => {
+    try {
+        const results = await db.query(
+            "UPDATE investor SET investorFirstName = $1, investorLastName = $2, investorStreetAddress = $3, investorContactNumber = $4, investorEmailAddress = $5 where investor_id = $6 returning *", 
+            [
+                req.body.investorFirstName, 
+                req.body.investorLastName, 
+                req.body.investorStreetAddress, 
+                req.body.investorContactNumber,
+                req.body.investorEmailAddress,
+                req.params.id
+            ]);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                investor: results.rows[0],
+            },    
+        })
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+// Delete a Investor 
+app.delete("/investor/:id", async (req, res) => {
+    try {
+        const result = db.query("DELETE FROM investor WHERE investor_id = $1", [req.params.id]);
         res.status(204).json({
             status: "success", 
         });
