8885d60dee21abc34a230155661b70761d9c61ff
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2023-03-30 14:24:48 +1300

    minor clean up

--- client/src/pages/Borrower.js
+++ client/src/pages/Borrower.js
@@ -37,7 +37,6 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { BorrowersContextProvider } from '../context/BorrowersContext';
 
 const theme = createTheme({
     typography: {
