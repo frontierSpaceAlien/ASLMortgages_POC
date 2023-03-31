1697b4bd8379bb839759997d0dcf7a07a5c95dc3
AngusJiang16 <tpj0583@autuni.ac.nz>
2023-03-31 20:13:11 +1300

    few changed on index

--- server/index.js
+++ server/index.js
@@ -30,13 +30,62 @@ app.post("/Investor", async (req, res) => {
     }
     
 })
+
 //get all investors
+app.get("/Investor", async (req, res) => {
+    try {
+      const allInvestors = await pool.query("SELECT * FROM Investor");
+      res.json(allInvestors.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
 
 //get a investor
+app.get("/Investor/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const Investor = await pool.query("SELECT * FROM Investor WHERE investor_id = $1", 
+      [id]);
+  
+      res.json(Investor.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
 
 //update a investor
+app.put("/Investor/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { description } = req.body;
+      const updateInvestor = await pool.query(
+        "UPDATE Investor SET description = $1 WHERE investor_id = $2",
+        [description, id]
+      );
+  
+      res.json("Investor was updated!");
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+  
 
 //delete a investor
+app.delete("/Investor/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteInvestor = await pool.query("DELETE FROM Investor WHERE investor_id = $1", [
+        id
+      ]);
+      res.json("Investor was deleted!");
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+  
 
 app.listen(5000, () => {
     //callback function to tell the server running
