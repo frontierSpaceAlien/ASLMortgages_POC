38bba7e238360f8229d23a1be8fccee058fb4ea6
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2023-04-01 18:14:15 +1300

    added cards and formatted the loan table

--- client/src/pages/Loan.js
+++ client/src/pages/Loan.js
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import NetflixSansReg from '../fonts/NetflixSans-Regular.ttf';
-import { DataGrid } from "@mui/x-data-grid";
-import { Typography } from "@mui/material";
+import { DataGrid, GridOverlay } from "@mui/x-data-grid";
+import { Grid, Typography } from "@mui/material";
 import Box from '@mui/material/Box';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 
 const theme = createTheme({
   typography: {
@@ -23,6 +29,15 @@ const theme = createTheme({
       }
     `,
       },
+      MuiDataGrid:{
+        styleOverrides:{
+          root:{
+            '&.MuiDataGrid-root .MuiDataGrid-columnHeader:focus, &.MuiDataGrid-root .MuiDataGrid-cell:focus': {
+              outline: 'none',
+          },
+          }
+        }
+      }
   },
 });
 
@@ -38,54 +53,235 @@ const StyledDataGrid = styled(DataGrid)((theme) => ({
   }));
 
   
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 2, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 3, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 4, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 5, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 6, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 7, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 8, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 9, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 10, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 11, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 12, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 13, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 14, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 15, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 16, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 17, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 18, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 19, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+    { id: 20, startdate: '10/03/2022', enddate: '10/09/2022' ,dayintdue: 10, loan: 'Stamos2022', intrate: 14.95,netadv: 326860.20 },
+
 ];
 
-function loanView({children, expandComponent, ...otherProps}){
-  
+const customRowOverlay = () =>{
+ return(
+  <GridOverlay>
+    <ErrorOutlineIcon></ErrorOutlineIcon>
+    <div>No Loan data found</div>
+  </GridOverlay>
+ )
 }
 
+const columns = [
+  { field: 'loan', headerName: 'Loan', flex: 1, headerClassName: 'super-app-theme--header', },
+  { field: 'startdate', headerName: 'Start Date', flex: 1, headerClassName: 'super-app-theme--header', },
+  { field: 'enddate', headerName: 'End Date', flex: 1, headerClassName: 'super-app-theme--header', },
+  { field: 'dayintdue', headerName: 'Day Interest Due', flex: 1, headerClassName: 'super-app-theme--header', },
+  { field: 'intrate', headerName: 'Interest Rate', flex: 1, headerClassName: 'super-app-theme--header', },
+  {
+    field: 'netadv',
+    headerName: 'Net Advanced',
+    headerClassName: 'super-app-theme--header',
+    flex: 1,
+    type: 'number',
+    width: 90,
+    headerAlign: 'left',
+    align: 'left'
+  },
+];
+
+
+function LoanView({children, component, ...otherProps}){
+
+  return(
+    <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Day Interest Due
+              </Typography>
+              <Typography variant="h9" component="div">
+                10
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Borrower
+              </Typography>
+              <Typography variant="h9" component="div">
+                John Stamos
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Investor(s)
+              </Typography>
+              <Typography variant="h9" component="div">
+                Robert Smith
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Interest
+              </Typography>
+              <Typography variant="h9" component="div">
+                $25,206.54
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Daily Interest
+              </Typography>
+              <Typography variant="h9" component="div">
+                $147.07
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Monthly Interest
+              </Typography>
+              <Typography variant="h9" component="div">
+                $0.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Management Fee
+              </Typography>
+              <Typography variant="h9" component="div">
+                $7,000.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Broker Fee
+              </Typography>
+              <Typography variant="h9" component="div">
+                $0.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Legal Fee
+              </Typography>
+              <Typography variant="h9" component="div">
+                $0.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Variation
+              </Typography>
+              <Typography variant="h9" component="div">
+                $0.00
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Total Repayable
+              </Typography>
+              <Typography variant="h9" component="div">
+                $359,066.74
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={2}>
+          <Card>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Capitalised
+              </Typography>
+              <Typography variant="h9" component="div">
+                Yes
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+
 export default function DataTable() {
-  const [dummyData, setData] = React.useState(rows);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const [selectedRows, setSelectedRows] = React.useState([]);
   
-  const columns = [
-    { field: 'firstName', headerName: 'First name', flex: 1, headerClassName: 'super-app-theme--header', },
-    { field: 'lastName', headerName: 'Last name', flex: 1, headerClassName: 'super-app-theme--header', },
-    {
-      field: 'age',
-      headerName: 'Age',
-      headerClassName: 'super-app-theme--header',
-      flex: 1,
-      type: 'number',
-      width: 90,
-    },
-  ];
-
-  const currentRows = dummyData.filter((r, ind) => {
-    return ind >= rowsPerPage * page && ind < rowsPerPage * (page + 1);
-  });
-
-
     return (
-      <div style={{ width: 1540, alignContent: "center", margin: "auto" }}>
+      <div style={{height: '100%',paddingLeft: 100, paddingRight:100}}>
         <div>
           <ThemeProvider theme  = {theme}>
             <Typography>
               <h2>
-                Loan info here extended
+                Loan Information
               </h2>
+              <LoanView>
+
+              </LoanView>
             </Typography>
           </ThemeProvider>
         </div>
@@ -95,22 +291,30 @@ export default function DataTable() {
             width: '100%',
             '& .super-app-theme--header': {
               backgroundColor: 'rgba(0,0,0 )',
-              color: "white"
+              color: "white",
+              flex: 1,
             },
           }}>
             <StyledDataGrid
-              sx ={{color: "black"}}
-              rows={currentRows}
+              sx ={{color: "black", marginTop: 4
+              }}
+              rows={rows}
               columns={columns}
+              slots={{ noRowsOverlay: customRowOverlay }}
+              {...rows}
+              initialState={{
+                ...rows.initialState,
+                pagination: { paginationModel: { pageSize: 20 } },
+              }}
+              pageSizeOptions={[]}
               autoHeight
-              pagination
-
+              getRowHeight={() => 'auto'}
 
               // this gets the all the information of a selected row.
               // check console for details
               onRowSelectionModelChange={(ids) => {
                 const selectedIDS = new Set(ids);
-                const selectedRows = currentRows.filter((row) =>
+                const selectedRows = rows.filter((row) =>
                   selectedIDS.has(row.id),
                 );
 
@@ -118,7 +322,7 @@ export default function DataTable() {
                 console.log(selectedRows)
               }}
 
-              {...currentRows}
+              {...rows}
               />
           </Box>
         </ThemeProvider>
